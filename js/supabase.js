c07afc4e1ebc5994b3c986d0d32ac9b6127f9104
0ri0546 <orio60@outlook.fr>
2026-09-08 03:29:36 +0200

    fix

--- js/supabase.js
+++ js/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = "https://uhgjitjnduetljcmtlb.supabase.co";
+const SUPABASE_URL = "https://uhgjtijinduetljcmtlb.supabase.co";
 const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_7U_OEbRLbfQzxVv9WLmtOQ__n3iFUJt";
 
 export const supabase = createClient(
